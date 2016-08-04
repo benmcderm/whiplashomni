c96df7e1ba81cf963d6d72101c6b9ce801a39497
benmcderm <benmcderm@gmail.com>
2016-08-03 17:16:26 -0700

    Another

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,10 @@
 class OrdersController < ApplicationController
   def index
     WhiplashApi::Base.testing!
-    @orders = WhiplashApi::Order.all
+    WhiplashApi::Base.api_version = 2
+    WhiplashApi::Base.api_key = 'c32801b46108ead59633ad62e6c3e3f3c865a4cb8d5dcfd930dd3635ea9da49d'
+    orders = WhiplashApi::Order.all
+
+    render :text => orders.inspect
   end
 end
