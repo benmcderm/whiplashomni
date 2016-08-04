f51fa9a65fab1bf6956ee5b5f3cd12c5e973a334
benmcderm <benmcderm@gmail.com>
2016-08-04 14:29:01 -0700

    Builder updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -36,8 +36,8 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    token = request.env['omniauth.auth']['credentials']['token'].to_s
-    cust_id = request.env['omniauth.auth']['extra']['raw_info']['customer_ids'][0].to_i
+    token = request.env['omniauth.auth']['credentials']['token']
+    cust_id = request.env['omniauth.auth']['extra']['raw_info']['customer_ids'][0]
     authy = "Token: " + token + "  Customer ID:" + cust_id
     WhiplashApi::Base.testing!
     WhiplashApi::Base.api_version = 2
@@ -45,7 +45,7 @@ class SessionsController < ApplicationController
     WhiplashApi::Base.api_key = token
     # orders = WhiplashApi::Order.all
 
-    render :text => authy
+    render :text => cust_id
   end
 
 
