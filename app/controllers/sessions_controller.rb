4cbd9334f686511f5d8cac6827ed3a21669b9409
benmcderm <benmcderm@gmail.com>
2016-08-04 11:40:41 -0700

    Secret:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    auth_hash = request.env['omniauth.auth']['credentials']['token']
+    auth_hash = 'Token' + request.env['omniauth.auth']['credentials']['token'] + 'Secret: ' + request.env['omniauth.auth']['credentials']['secret']
 
     render :text => auth_hash
   end
 
   # def create
   #   WhiplashApi::Base.testing!
-  #   WhiplashApi::Base.api_key = '23447e1eaeddf2d1c4af4c9cf88524af2863cb1f72d500dd9328b34735a3f3b0' #request.env['omniauth.auth']['credentials']['token']
+  #   WhiplashApi::Base.api_key = request.env['omniauth.auth']['credentials']['token']
   #   orders = WhiplashApi::Order.all
   #   render :text => orders
   # end
