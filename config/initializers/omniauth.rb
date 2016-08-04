76c137402f024215100bc79e364815656ee44bfa
benmcderm <benmcderm@gmail.com>
2016-08-04 13:05:14 -0700

    Secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -45,5 +45,5 @@ module OmniAuth
   end
 end
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :whiplash, ENV['WHIPLASH_CLIENT_ID'], ENV['WHIPLASH_CLIENT_SECRET'], scope: 'read_orders read_user'
+  provider :whiplash, ENV['WHIPLASH_CLIENT_ID'], ENV['WHIPLASH_CLIENT_SECRET'], scope: 'read_orders write_orders read_items write_items read_web_hooks write_web_hooks read_customers read_user'
 end
