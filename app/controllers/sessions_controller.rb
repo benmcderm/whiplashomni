a73cca76dc06ba61183109f3f489a314f89fb955
benmcderm <benmcderm@gmail.com>
2016-08-04 11:54:32 -0700

    Secret

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,30 @@
 class SessionsController < ApplicationController
-  def create
-    auth_hash = 'Token' + request.env['omniauth.auth']['credentials']['token'] + 'Secret: ' + request.env['omniauth.auth']['credentials']['secret']
-
-    render :text => auth_hash
-  end
+  # def create
+  # auth_hash = 'Token' + request.env['omniauth.auth']['credentials']['token'] + 'Secret: ' + request.env['omniauth.auth']['credentials']['secret']
+  #
+  #   render :text => auth_hash
+  # end
 
   # def create
-  #   WhiplashApi::Base.testing!
-  #   WhiplashApi::Base.api_key = request.env['omniauth.auth']['credentials']['token']
-  #   orders = WhiplashApi::Order.all
+  # WhiplashApi::Base.testing!
+  # WhiplashApi::Base.api_key = request.env['omniauth.auth']['credentials']['token']
+  # orders = WhiplashApi::Order.all
   #   render :text => orders
   # end
 
 
 
 
-  # def create
-  #   auth = request.env['rack.auth']
-  #   unless @auth = Authorization.find_from_hash(auth)
-  #     @auth = Authorization.create_from_hash(auth, current_user)
-  #   end
-  #   self.current_user = @auth.user
-  #
-  #   render 'orders/index'
-  # end
+  def create
+    auth = request.env['rack.auth']['credentials']
+    unless @auth = Authorization.find_from_hash(auth)
+      @auth = Authorization.create_from_hash(auth, current_user)
+    end
+    self.current_user = @auth.user
+    WhiplashApi::Base.testing!
+    WhiplashApi::Base.api_version = 2
+    WhiplashApi::Base.api_key = request.env['rack.auth']['credentials']['token']
+    @orders = WhiplashApi::Order.all
+    render 'orders/index'
+  end
 end
