a831d2b60f6b88d80921f6b5b66e12cbf92b7056
benmcderm <benmcderm@gmail.com>
2016-08-03 17:23:20 -0700

    Another

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
-    render json: request.env['omniauth.auth']
     WhiplashApi::Base.testing!
+    WhiplashApi::Base.api_version = 2
     WhiplashApi::Base.api_key = auth_hash['credentials']['token']
     orders = WhiplashApi::Order.all
 
-    render :text => orders.inspect
+    render :text => orders
     # auth_hash = request.env['omniauth.auth']['credentials']['token']
     # WhiplashApi::Base.testing!
     # WhiplashApi::Base.api_version = 2
