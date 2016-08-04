4da4ce57c8e31a4eb636be6c3bd2040e917fd982
benmcderm <benmcderm@gmail.com>
2016-08-04 15:51:06 -0700

    Testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,20 @@
 class SessionsController < ApplicationController
 
+  def create
+    auth_hash = request.env['omniauth.auth']
+    token = auth_hash['credentials']['token']
+    cust_id = auth_hash['extra']['raw_info']['customer_ids'][0]
+    WhiplashApi::Base.api_version = 2
+    WhiplashApi::Base.customer_id = cust_id
+    WhiplashApi::Base.api_key = token
+    @orders = WhiplashApi::Order.all
+
+    render 'orders/index'
+  end
+  # 
   # def create
-  #   auth_hash = request.env['omniauth.auth']
-  #   token = auth_hash['credentials']['token']
-  #   cust_id = auth_hash['extra']['raw_info']['customer_ids'][0]
+  #   token = '23447e1eaeddf2d1c4af4c9cf88524af2863cb1f72d500dd9328b34735a3f3b0'
+  #   cust_id = 242
   #   WhiplashApi::Base.testing!
   #   WhiplashApi::Base.api_version = 2
   #   WhiplashApi::Base.customer_id = cust_id
@@ -12,17 +23,5 @@ class SessionsController < ApplicationController
   #
   #   render 'orders/index'
   # end
-
-  def create
-    token = '23447e1eaeddf2d1c4af4c9cf88524af2863cb1f72d500dd9328b34735a3f3b0'
-    cust_id = 242
-    WhiplashApi::Base.testing!
-    WhiplashApi::Base.api_version = 2
-    WhiplashApi::Base.customer_id = cust_id
-    WhiplashApi::Base.api_key = token
-    @orders = WhiplashApi::Order.all
-
-    render 'orders/index'
-  end
 
 end
