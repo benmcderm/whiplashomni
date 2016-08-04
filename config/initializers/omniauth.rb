961523a08026129b62ee1ab2a0873f5a79d12587
benmcderm <benmcderm@gmail.com>
2016-08-04 13:30:44 -0700

    Builder updates

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,6 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :whiplash, ENV['WHIPLASH_CLIENT_ID'], ENV['WHIPLASH_CLIENT_SECRET'], scope: 'read_orders write_orders read_items write_items read_web_hooks write_web_hooks read_customers read_user',   client_options: {
-      site: 'https://testing.whiplashmerch.com',
-      authorize_url: 'https://testing.whiplashmerch.com/login'
+      site: 'https://testing.whiplashmerch.com'
     }
 end
